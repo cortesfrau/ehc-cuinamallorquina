213c5a7066f1114b984d657d4b803fc1a857839a
Lluís Cortès <lluiscortes@outlook.com>
2021-05-14 17:39:16 +0200

    Commit

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+// Custom Functions
 import {headerToggle} from './src/js/custom';
 import {activeLinks} from './src/js/custom';
+
+// Simple LightBox
+import SimpleLightbox from 'simplelightbox';
+
 
 window.onload = function() {
   headerToggle();
   activeLinks();
+
+
+  // Lightbox
+  var lightbox = new SimpleLightbox('.gallery-item');
+
+
+
+  // Is Element Visible?
+  var isInViewport = function(elem) {
+    var distance = elem.getBoundingClientRect();
+    return (
+      distance.top >= 0 &&
+      distance.left >= 0 &&
+      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+// read the link on how above code works
+
+  var findMe = document.querySelectorAll('.animated');
+
+  window.addEventListener('scroll', function(event) {
+// add event on scroll
+    findMe.forEach(element => {
+      //for each .thisisatest
+      if (isInViewport(element)) {
+        //if in Viewport
+        element.classList.add('animate');
+      }
+    });
+  }, false);
 }
